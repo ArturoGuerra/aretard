a22967883058e9a2b0f83cbc5889226a47440ee3
Arturo Guerra <ar2roguerra@gmail.com>
2020-04-14 19:55:04 +0000

    we did stuff

--- bot/bot.py
+++ bot/bot.py
@@ -4,4 +4,4 @@ class Bot(commands.Bot):
     def __init__(self, config, mem_store):
         super().__init__(command_prefix=config.prefix)
         self.config = config
-        self.mem_store = mem_store    
+        self.mem_store = mem_store
--- main.py
+++ main.py
@@ -3,6 +3,12 @@ from stores import MemStore
 
 from cogs import register
 
+import asyncio
+
+# Makes use a uvloop which is written is C for much better performance
+import uvloop
+asyncio.set_event_loop_policy(uvloop.EventLoopPolicy())
+
 config = Config.Create()
 mem_store = MemStore()
 bot = Bot(config, mem_store)
@@ -10,9 +16,14 @@ bot = Bot(config, mem_store)
 register(bot)
 
 if __name__ == "__main__":
+    loop = asyncio.get_event_loop()
+    loop.set_debug(True)
+
+    bot.loop = loop
+    
     try:
-        bot.loop.run_until_complete(bot.start(bot.config.token, reconnect=True))
+        loop.run_until_complete(bot.start(bot.config.token, reconnect=True))
     except KeyboardInterrupt:
-        bot.loop.run_until_complete(bot.logout())
+        loop.run_until_complete(bot.logout())
     finally:
-        bot.loop.close()
+        loop.close()
