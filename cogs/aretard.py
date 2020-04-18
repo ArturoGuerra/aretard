04f6e59ee607131fa24175844472c7e4ea704c3c
Arturo Guerra <ar2roguerra@gmail.com>
2020-04-18 07:16:09 +0000

    KILL ME

--- cogs/aretard.py
+++ cogs/aretard.py
@@ -5,23 +5,22 @@ seed(1)
 class Aretard(Plugin):
     def __init__(self, bot):
         super().__init__(bot)
-        self.randomstuffs = ["https://media.discordapp.net/attachments/296056831514509312/658820601485459486/video0.mp4", "https://cdn.discordapp.com/attachments/584886538400432154/584890724160503849/Slingshot.mov", "https://media.discordapp.net/attachments/296056831514509312/662123318052257816/oof_2.mp4",
+        self.optimus_stuff = ["https://i.arturonet.com/DTH1pTMnNy.gif", "https://media.discordapp.net/attachments/296056831514509312/680099251258130447/carmechanic.mp4",
+         "https://cdn.discordapp.com/attachments/296056831514509312/700947734026387556/soy_libre.mp4", "https://cdn.discordapp.com/attachments/613025217974894592/691471262307975247/video0.mp4"]
+        self.randomstuffs = ["https://cdn.discordapp.com/attachments/602312656195551232/700963725598785536/aDgpKqd_460s.png", "https://media.discordapp.net/attachments/296056831514509312/658820601485459486/video0.mp4", "https://cdn.discordapp.com/attachments/584886538400432154/584890724160503849/Slingshot.mov", "https://media.discordapp.net/attachments/296056831514509312/662123318052257816/oof_2.mp4",
 "https://cdn.discordapp.com/attachments/655516260804853791/661806087145783296/video0_2.mp4", "https://cdn.discordapp.com/attachments/655516260804853791/661401266437619742/walter_at_the_bakery_bread1.mp4",
 "https://cdn.discordapp.com/attachments/655516260804853791/658939294236540949/canoe.mp4", "https://cdn.discordapp.com/attachments/655516260804853791/657118526737481729/icecream_truck.mp4"]
 
 
     @command(name="optimus")
     async def optimus(self, ctx):
+        max_idx = len(self.optimus_stuff) - 1
+        idx = randint(0, max_idx)
+
         try:
-            await ctx.send("https://i.arturonet.com/DTH1pTMnNy.gif")
+            await ctx.send(self.optimus_stuff[idx])
         except: pass
 
-    @command(name="optimus2")
-    async def optimus2(self, ctx):
-        try:
-            await ctx.send("https://media.discordapp.net/attachments/296056831514509312/680099251258130447/carmechanic.mp4")
-        except: pass
-    
     @command(name="flips")
     async def flips(self, ctx):
         try:
@@ -53,7 +52,7 @@ class Aretard(Plugin):
         try:
             await ctx.send("https://i.redd.it/1e35a6ui9et41.jpg")
         except: pass
-        
+
     @command(name="random")
     async def random(self, ctx):
         max_idx = len(self.randomstuffs) - 1
