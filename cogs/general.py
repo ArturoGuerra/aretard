2e2fabd099d5495b097151da77a69553069d2d9d
Arturo Guerra <ar2roguerra@gmail.com>
2020-04-18 06:59:22 +0000

    Stuffs

--- cogs/general.py
+++ cogs/general.py
@@ -7,6 +7,6 @@ class General(Plugin):
     @command(name="addme")
     async def addme(self, ctx):
         try:
-            await ctx.send("https://discordapp.com/oauth2/authorize?client_id=676207077013585951&scope=bot&permissions=117760")
+            await ctx.send("https://discordapp.com/oauth2/authorize?client_id=700960910201585704&scope=bot&permissions=117760")
         except: pass
     
